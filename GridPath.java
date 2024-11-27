9da144b6c682d2e3c28b7614daf67ee9afe7c930
aidant11 <aidant10@nycstudents.net>
2024-11-27 16:29:01 +0000

    123

--- GridPath.java
+++ GridPath.java
@@ -1,1 +1,27 @@
+public class GridPath {
+    private int[][] grid;
+
+    public Location getNextLoc(int row, int col) {
+        return null;
+    }
+
+    public int sumPath(int row, int col) {
+        return 0;
+    }
+
+    public GridPath(int[][] values) {
+        grid = values;
+    }
+
+    public String toString() {
+        String s = "";
+        for (int[] row:grid) {
+            for (int col:row) {
+                s += col + " ";
+            }
+            s += "\n";
+        }
+        return s;
+    }
+}
 
